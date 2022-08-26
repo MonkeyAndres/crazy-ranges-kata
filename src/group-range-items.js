d56da334ece855cff929f2ae1879c4296ac3ea0e
Andres Martin Angulo <monkeyandres@protonmail.com>
2022-08-26 11:21:53 +0200

    feat: submit group range items solution

--- src/group-range-items.js
+++ src/group-range-items.js
@@ -1,5 +1,103 @@
+const NUMBER_COMPONENT_REGEXP = /^\d+$/
+const PREFIX_COMPONENT_REGEXP = /^([a-z]{1})(\d+)$/i
+const SUFFIX_COMPONENT_REGEXP = /^(\d+)([a-z]{1})$/i
+
+const groupBy = (keyExtractor, list) => {
+  const groups = list.reduce((acc, item) => {
+    const groupKey = keyExtractor(item)
+
+    if (!acc[groupKey]) {
+      acc[groupKey] = [item]
+    } else {
+      acc[groupKey].push(item)
+    }
+
+    return acc
+  }, {})
+
+  return Object.values(groups)
+}
+
+const getBucketKeyForItem = (item) => {
+  if (item.prefix) {
+    return `prefix-${item.prefix}`
+  }
+
+  if (item.suffix) {
+    return `suffix-${item.suffix}`
+  }
+
+  return 'numbers'
+}
+
+const parseRangeItem = (item) => {
+  if (NUMBER_COMPONENT_REGEXP.test(item)) {
+    return { number: parseInt(item, 10) }
+  }
+
+  if (PREFIX_COMPONENT_REGEXP.test(item)) {
+    const [, prefix, number] = item.match(PREFIX_COMPONENT_REGEXP)
+    return { prefix, number: parseInt(number, 10) }
+  }
+
+  if (SUFFIX_COMPONENT_REGEXP.test(item)) {
+    const [, number, suffix] = item.match(SUFFIX_COMPONENT_REGEXP)
+    return { number: parseInt(number, 10), suffix }
+  }
+}
+
+const numbersToRange = (numbers) => {
+  const sortedNumbers = numbers.sort((a, b) => a - b)
+
+  let rangeStart = sortedNumbers[0]
+  let lastRangeItem = sortedNumbers[0]
+
+  return sortedNumbers.reduce((acc, _, index) => {
+    const next = sortedNumbers[index + 1]
+
+    if (next === lastRangeItem || next === lastRangeItem + 1) {
+      lastRangeItem = next
+      return acc
+    }
+
+    const oneNumberRange = rangeStart === lastRangeItem
+    const range = oneNumberRange
+      ? `${lastRangeItem}`
+      : [rangeStart, lastRangeItem].join('-')
+
+    rangeStart = next
+    lastRangeItem = next
+
+    return [...acc, range]
+  }, [])
+}
+
 const groupRangeItems = (input) => {
-  return true
+  const bucketedItems = groupBy(getBucketKeyForItem, input.map(parseRangeItem))
+
+  return bucketedItems.flatMap((group) => {
+    const groupNumbers = group.map((item) => item.number)
+    const numberRanges = numbersToRange(groupNumbers)
+
+    const firstItem = group[0]
+
+    return numberRanges.map((range) =>
+      range
+        .split('-')
+        .map((extreme) => {
+          if (firstItem.prefix) {
+            return `${firstItem.prefix}${extreme}`
+          }
+
+          if (firstItem.suffix) {
+            return `${extreme}${firstItem.suffix}`
+          }
+
+          return extreme
+        })
+        .join('-'),
+    )
+  })
 }
 
 module.exports = { groupRangeItems }
--- src/group-range-items.test.js
+++ src/group-range-items.test.js
@@ -7,46 +7,46 @@ const scenarios = [
   { enabled: true, input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], expected: ['1-10'] },
 
   // PREFIX
-  { enabled: false, input: ['A1', 'A2', 'A3'], expected: ['A1-A3'] },
+  { enabled: true, input: ['A1', 'A2', 'A3'], expected: ['A1-A3'] },
   {
-    enabled: false,
+    enabled: true,
     input: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10'],
     expected: ['A1-A10'],
   },
 
   // SUFIX
-  { enabled: false, input: ['1A', '2A', '3A'], expected: ['1A-3A'] },
+  { enabled: true, input: ['1A', '2A', '3A'], expected: ['1A-3A'] },
   {
-    enabled: false,
+    enabled: true,
     input: ['1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A', '10A'],
     expected: ['1A-10A'],
   },
 
   // MULTI-LETTER RANGE
   {
-    enabled: false,
+    enabled: true,
     input: ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4'],
     expected: ['A1-A4', 'B1-B4'],
   },
   {
-    enabled: false,
+    enabled: true,
     input: ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'],
     expected: ['A1-A3', 'B1-B3', 'C1-C3'],
   },
   {
-    enabled: false,
+    enabled: true,
     input: ['1A', '1B', '2A', '2B', '3A', '3B', '4A', '4B'],
     expected: ['1A-4A', '1B-4B'],
   },
   {
-    enabled: false,
+    enabled: true,
     input: ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C'],
     expected: ['1A-3A', '1B-3B', '1C-3C'],
   },
 
   // FINAL BOSS
   {
-    enabled: false,
+    enabled: true,
     input: [
       '10A',
       '11A',
